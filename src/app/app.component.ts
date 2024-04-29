02addbeac81fd8d5f52c8474670c6fbf2dd9e135
pokuong lao <pokuonglao123@gmail.com>
2024-04-29 15:27:03 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  PhoneNumber = '262-454-4111';
+  Email = 'pochinglao@gmail.com';
+}
